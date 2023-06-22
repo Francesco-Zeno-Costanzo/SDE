4929ab332cdff8321b295b506e969c76de947b21
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-06-22 22:43:32 +0200

    Update GMB.py

--- GMB.py
+++ GMB.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 # geometric Brownian motion
 # Heun method
 
-def Heun(N, tf, y0, f, g, dg):
+def Heun(N, tf, y0, f, g, dg, args_f=(), args_g=(), args_dg=()):
     '''
     Heun method for SDE: dx = f*dt + g*dw
-    
+
     Parameters
     ----------
     N : int
@@ -20,7 +20,13 @@ def Heun(N, tf, y0, f, g, dg):
         function in the equation
     dg : callable
         derivative of g
-    
+    args_f : tuple, optional
+        extra argument to pass at f
+    args_g : tuple, optional
+        extra argument to pass at g
+    args_dg : tuple, optional
+        extra argument to pass at dg
+
     Returns
     -------
     ts : 1darray
@@ -30,7 +36,7 @@ def Heun(N, tf, y0, f, g, dg):
     '''
 
     dt = tf/N  # time step
-    
+
     ts = np.zeros(N + 1)
     ys = np.zeros(N + 1)
 
@@ -38,36 +44,40 @@ def Heun(N, tf, y0, f, g, dg):
 
     for i in range(N):
         dW = np.random.normal(loc=0.0, scale=np.sqrt(dt)) # winer process
-        
-        y0 = ys[i] + f(ys[i])*dt + g(ys[i])*dW + 0.5*g(ys[i])*dg()*dW**2
-        y1 = ys[i] + f(y0)   *dt + g(y0)   *dW + 0.5*g(y0)   *dg()*dW**2
-        
+
+        y0 = ys[i] + f(ys[i], *args_f)*dt + g(ys[i], *args_g)*dW + 0.5*g(ys[i], *args_g)*dg(ys[i], *args_dg)*dW**2
+        y1 = ys[i] + f(y0   , *args_f)*dt + g(y0   , *args_g)*dW + 0.5*g(y0   , *args_g)*dg(y0   , *args_dg)*dW**2
+
         ys[i+1] = 0.5*(y0 + y1)
         ts[i+1] = ts[i] + dt
-    
+
     return ts, ys
 
+
 if __name__ == '__main__':
-    def f(z):
-        mu = 1
+
+    def f(z, mu):
+
         return mu*z
 
-    def g(z):
-        sigma = 0.5
+    def g(z, sigma):
+
         return sigma*z
 
-    def dg():
-        sigma = 0.5
+    def dg(z, sigma):
+
         return sigma
 
-    
+
     # Parameter of simulation
     N = 10000
     tf = 4
     y0 = 3
+    mu = 1
+    sigma = 0.5
 
     for _ in range(5):
-        ts, ys = Heun(N, tf, y0, f, g, dg)
+        ts, ys = Heun(N, tf, y0, f, g, dg, args_f=(mu,), args_g=(sigma,), args_dg=(sigma,))
         plt.plot(ts, ys)
 
     plt.figure(1)
